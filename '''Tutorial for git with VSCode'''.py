08f8ca8c9a15bac91639ce33e75a94e02e60304a
tokilava <tokilava@gmail.com>
2022-09-13 19:34:33 +0200

    some new quotes on the main branch

--- '''Tutorial for git with VSCode'''.py
+++ '''Tutorial for git with VSCode'''.py
@@ -5,8 +5,8 @@ print("I bet the next line is going to be Hello world in the terminal...")
 print("Hello world")
 a = 10
 b = 20
-print(a+b)
-
+print("What's the answer to the universe and everything?")
+print("42!")
 "This is a new text, that I just wrote"
 
 "Trying a new comment on the main branch"
