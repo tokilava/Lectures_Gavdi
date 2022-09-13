d20a10b035a48e46478dee79be81de36bd0e669c
tokilava <tokilava@gmail.com>
2022-09-13 19:37:14 +0200

    removed something

--- '''Tutorial for git with VSCode'''.py
+++ '''Tutorial for git with VSCode'''.py
@@ -6,7 +6,7 @@ print("Hello world")
 a = 10
 b = 20
 print("What's the answer to the universe and everything?")
-print("42!")
+print("42")
 "This is a new text, that I just wrote"
 
 "Trying a new comment on the main branch"
